f5a06ff0c40039de4f62a288219c8eb8655cdc2b
yusaku-univ <sakusaku71@icloud.com>
2025-05-27 14:45:03 +0900

    main.c

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,27 @@
 /*   By: mech-user <mech-user@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 14:11:40 by mech-user         #+#    #+#             */
-/*   Updated: 2025/05/27 14:11:41 by mech-user        ###   ########.fr       */
+/*   Updated: 2025/05/27 14:44:51 by mech-user        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("test.txt", O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (0);
+}
