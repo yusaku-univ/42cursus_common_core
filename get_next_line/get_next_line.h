89d61be9469071932209eab5cdea63ad596be6a9
Yusaku <sakusaku71@icloud.com>
2025-06-03 15:23:36 +0900

    first

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,27 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mech-user <mech-user@student.42.fr>        +#+  +:+       +#+        */
+/*   By: yusaku <yusaku@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 14:11:42 by mech-user         #+#    #+#             */
-/*   Updated: 2025/05/27 15:41:52 by mech-user        ###   ########.fr       */
+/*   Updated: 2025/05/28 10:22:12 by yusaku           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <unistd.h>
 
-char *get_next_line(int fd);
-char *ft_strjoin(char const *s1, char const *s2);
-char *ft_strchr(const char *string, int c);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int c);
 
-void ft_bzero(void *s, size_t n);
-void ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
+void	ft_calloc(size_t count, size_t size);
 
-size_t ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
 
-#endif GET_NEXT_LINE_H
+#endif
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -3,13 +3,94 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mech-user <mech-user@student.42.fr>        +#+  +:+       +#+        */
+/*   By: yusaku <yusaku@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 14:11:46 by mech-user         #+#    #+#             */
-/*   Updated: 2025/05/27 15:09:43 by mech-user        ###   ########.fr       */
+/*   Updated: 2025/05/28 10:23:54 by yusaku           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strjoin()
+char	*ft_strjoin(char const *str1, char const *str2)
+{
+	int		totalsize;
+	char	*result;
+	int		i;
+	int		j;
+
+	totalsize = ft_strlen(str1) + ft_strlen(str2);
+	i = 0;
+	result = malloc(sizeof(char) * (totalsize + 1));
+	if (!result || !str1 || !str2)
+		return (NULL);
+	while (str1[i])
+	{
+		result[i] = str1[i];
+		i++;
+	}
+	j = 0;
+	while (str2[j])
+	{
+		result[i] = str2[j];
+		i++;
+		j++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	char	*string;
+	size_t	i;
+
+	string = (char *)string;
+	i = 0;
+	while (string[i] != c && string[i])
+		i++;
+	if (string[i] == c)
+		return (&string[i]);
+	else
+		return (NULL);
+}
+
+void	ft_bzero(void *s, size_t size)
+{
+	size_t	i;
+	char	*ptr;
+
+	ptr = (char *)s;
+	i = 0;
+	while (i < size)
+	{
+		ptr[i] = '\0';
+		i++;
+	}
+}
+
+char	*ft_calloc(size_t count, size_t size)
+{
+	char	*ptr;
+	size_t	total;
+	size_t	i;
+
+	total = count * size;
+	if (count != 0 && total / count != size)
+		return (NULL);
+	ptr = malloc(total);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, total);
+	return (ptr);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
